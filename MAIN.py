639f5aa56f7d0d22b81b5fa0fe87d754dbf42fb9
Hezron Na <hezron.w12@gmail.com>
2024-02-29 22:50:38 +0300

    Committed 2024 0Th.225038

--- MAIN.py
+++ MAIN.py
@@ -432,21 +432,21 @@ def chat(widget):
     bg_color = 'lightgreen'
     fg_color = 'black'
 
-    nav_bar_color = 'lightblue'
+    nav_bar_bg_color = 'lightblue'
 
-    navbar = tk.Frame(chatbot_widget, bg='blue', borderwidth=0, border=0)
+    navbar = tk.Frame(chatbot_widget, bg=nav_bar_bg_color, borderwidth=0, border=0)
     navbar.place(relheight=0.03, relwidth=1, rely=0, relx=0)
 
     font_ = tk.Frame(navbar, bg=bg_color, borderwidth=2, border=0)
     font_.place(relheight=0.70, relwidth=0.2, rely=0.15, relx=0.02)
 
-    font_style_entry = tk.Entry(font_, bg='blue', fg=fg_color, relief=tk.GROOVE, font=("Times New Roman", 13), borderwidth=0, border=1)
+    font_style_entry = tk.Entry(font_, bg=nav_bar_bg_color, fg=fg_color, relief=tk.GROOVE, font=("Times New Roman", 13), borderwidth=0, border=1)
     font_style_entry.place(relheight=0.8, relwidth=0.7, rely=0.1, relx=0)
 
-    font_style_btn = tk.Button(font_, text='v', bg='blue',  fg=fg_color, relief=tk.GROOVE, font=("Times New Roman", 13, 'bold'), borderwidth=0, border=0)
-    font_style_btn.place(relheight=0.8, relwidth=0.1, rely=0.1, relx=0.7)
+    font_style_btn = tk.Button(font_, text='v', bg=nav_bar_bg_color, activebackground=nav_bar_bg_color, fg=fg_color, relief=tk.GROOVE, font=("Times New Roman", 13, 'bold'), borderwidth=0, border=1)
+    font_style_btn.place(relheight=0.8, relwidth=0.09, rely=0.1, relx=0.7)
 
-    font_size = tk.Entry(font_,  bg='blue', fg=fg_color, relief=tk.GROOVE, font=("Times New Roman", 11), borderwidth=0, border=1)
+    font_size = tk.Entry(font_,  bg=nav_bar_bg_color, fg=fg_color, relief=tk.GROOVE, font=("Times New Roman", 11), borderwidth=0, border=1)
     font_size.place(relheight=0.8, relwidth=0.19, rely=0.1, relx=0.8)
 
 
