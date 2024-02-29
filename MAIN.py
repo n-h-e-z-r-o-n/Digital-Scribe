b4a9098e8c0c8b6219c860564c430d5ee30e264d
Hezron Na <hezron.w12@gmail.com>
2024-02-29 12:29:53 +0300

    Committed 2024 0Th.122953

--- MAIN.py
+++ MAIN.py
@@ -630,11 +630,13 @@ def settings(widget):
     gradient_access_widget = tk.Entry(g1, borderwidth=0, border=1, font=("Calibri", 10))
     gradient_access_widget.place(relheight=0.07, relwidth=0.74, rely=0.071, relx=0.25)
     gradient_access_widget.insert(0, gradient_ai_access_key)
+    gradient_access_widget.bind('<Return>', lambda e: save_keys(gradient_access_widget.get(), gradient_work_widget.get(), assembly_widget.get()))
 
     tk.Label(g1, text="GRADIENT_WORKSPACE_ID :", font=("Calibri", 10, 'bold'), anchor='w', borderwidth=0, border=0).place(relheight=0.07, relwidth=0.24, rely=0.142, relx=0)
     gradient_work_widget = tk.Entry(g1, borderwidth=0, border=1, font=("Calibri", 10))
     gradient_work_widget.place(relheight=0.07, relwidth=0.74, rely=0.142, relx=0.25)
     gradient_work_widget.insert(0, gradient_ai_workspace_id)
+    gradient_work_widget.bind('<Return>', lambda e: save_keys(gradient_access_widget.get(), gradient_work_widget.get(), assembly_widget.get()))
 
     tk.Label(g1, text="assemblyai ", font=("Calibri", 12, 'bold'), anchor='w', borderwidth=0, border=0).place(relheight=0.07, relwidth=0.4, rely=0.22, relx=0)
     tk.Label(g1, text="assemblyai access key:", font=("Calibri", 10, 'bold'), anchor='w', borderwidth=0, border=0).place(relheight=0.07, relwidth=0.24, rely=0.3, relx=0)
