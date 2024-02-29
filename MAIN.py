584b087a8034aba7bb720cab070c3b2e205cfcd1
Hezron Na <hezron.w12@gmail.com>
2024-02-29 10:21:57 +0300

    Committed 2024 0Th.102157

--- MAIN.py
+++ MAIN.py
@@ -66,9 +66,9 @@ def access_keys_info():
      keys = shelve.open("keys.db")
      #try:
      if keys['access_keys'] == True:
-         gradient_ai_access_key = keys['gradient_ai_access_key']
-         gradient_ai_workspace_id = keys['gradient_ai_workspace_id']
-         assemblyai_access_key = keys['assemblyai_access_key']
+         gradient_ai_access_key = keys['-GA_']
+         gradient_ai_workspace_id = keys['-GW__']
+         assemblyai_access_key = keys['_AAI_']
 
          print('00 :',gradient_ai_workspace_id)
          print('00 :',gradient_ai_access_key)
