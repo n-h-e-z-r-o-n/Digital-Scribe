51ceb7304ffcca2ac6217ab3beaaf07d4a287cc0
Hezron Na <hezron.w12@gmail.com>
2024-03-01 00:26:18 +0300

    Committed 2024 0Fr. 02618

--- MAIN.py
+++ MAIN.py
@@ -492,8 +492,8 @@ def chat(widget):
 
     threading.Thread(target=font_change, args=(font_style_entry, font_size_entry, t1,)).start()
 
-    t1 = tk.Frame(chatbot_widget, bg=bg_color, relief=tk.SUNKEN, borderwidth=2, border=5)
-    t1.place(relheight=0.70, relwidth=0.79, rely=0.03, relx=0.78)
+    entity_section = tk.Frame(chatbot_widget, bg=bg_color, relief=tk.SUNKEN, borderwidth=2, border=5)
+    t1.place(relheight=0.70, relwidth=0.21, rely=0.03, relx=0.78)
 
     return chatbot_widget
 
