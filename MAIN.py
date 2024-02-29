3468ee10c4b754f96c3f3573a4679d5c1ce0cd1b
Hezron Na <hezron.w12@gmail.com>
2024-03-01 00:28:29 +0300

    Committed 2024 0Fr. 02829

--- MAIN.py
+++ MAIN.py
@@ -495,7 +495,7 @@ def chat(widget):
     entity_section = tk.Frame(chatbot_widget, bg=bg_color, relief=tk.SUNKEN, borderwidth=2, border=5)
     entity_section.place(relheight=0.70, relwidth=0.21, rely=0.03, relx=0.78)
 
-    tk.Frame(entity_section, bg='blue', relief=tk.SUNKEN, borderwidth=2, border=5).pack
+    tk.Frame(entity_section, bg='blue', relief=tk.SUNKEN, borderwidth=2, border=5).pack()
 
     return chatbot_widget
 
