a20fb65c3b2ac5bbf0dac6e5cd43b1b771af374e
Hezron Na <hezron.w12@gmail.com>
2024-03-01 00:09:53 +0300

    Committed 2024 0Fr. 00953

--- MAIN.py
+++ MAIN.py
@@ -444,7 +444,7 @@ def chat(widget):
                     try:
                         widget3.config(font=(font_style, font_size))
                         defalt_font_style = font_style.strip()
-                        defalt_font_size 
+                        defalt_font_size = int(font_size)
                         print('changed')
                     except:
                         pass
