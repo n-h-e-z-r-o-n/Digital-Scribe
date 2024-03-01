b13340a76146f5d3796da59e4adf3429f6c8fb8a
Hezron Na <hezron.w12@gmail.com>
2024-03-01 16:51:24 +0300

    Committed 2024 0Fr.165124

--- A_app.py
+++ A_app.py
@@ -1,6 +1,6 @@
 from gradientai import ExtractParamsSchemaValueType, Gradient
 
-
+mygradient = Gradient()
 
 schema = '{ "131313" : { "type": ExtractParamsSchemaValueType.NUMBER, "required": True, }, "ddfsf" : { "type": ExtractParamsSchemaValueType.STRING, "required": True, }, "dfg" : { "type": ExtractParamsSchemaValueType.STRING, "required": True, }, }'
 
--- speach_recognition.py
+++ speach_recognition.py
@@ -14,6 +14,15 @@ transcript = transcriber.transcribe(audio_url)
 
 print(transcript.text)
 
+document = (
+    "When Apple released the Apple Watch in 2015, it was business as "
+    + "usual for a company whose iPhone updates had become cultural "
+    + "touchstones. Before the watch went on sale, Apple gave early "
+    + "versions of it to celebrities like Beyoncé, featured it in fashion "
+    + "publications like Vogue and streamed a splashy event on the "
+    + "internet trumpeting its features."
+)
+
 
 
 """
