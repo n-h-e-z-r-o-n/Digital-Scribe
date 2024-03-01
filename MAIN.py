8ac4bfbdb9c822f95247a1e4c6305d0d0968c218
Hezron Na <hezron.w12@gmail.com>
2024-03-01 13:24:37 +0300

    Committed 2024 0Fr.132437

--- MAIN.py
+++ MAIN.py
@@ -501,7 +501,7 @@ def chat(widget):
     fr.place(relheight=0.97, relwidth=1, rely=0, relx=0)
     user_page_widget, user_page_root = attach_scroll(fr)
     fr2 = tk.Frame(user_page_widget, bg=bg_color, borderwidth=0, border=0, height=4000, width=int(screen_width*0.9747*0.21))
-    fr2.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+    fr2. pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
 
 
