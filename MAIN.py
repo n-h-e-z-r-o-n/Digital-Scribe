b6c4b2644a2795c563d0ae9bef4fb1cdcd07e346
Hezron Na <hezron.w12@gmail.com>
2024-03-01 14:31:06 +0300

    Committed 2024 0Fr.143106

--- MAIN.py
+++ MAIN.py
@@ -425,7 +425,7 @@ def Login_Section_widget(widget, root_widget):
 
 
 def chat(widget):
-    
+
     entity_lists = []
     def font_change(widget1, widget2, widget3):
         global defalt_font_style, defalt_font_size, closed
