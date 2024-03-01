4438b397efffb0eed8660d5ac6bd72e87dc4d420
Hezron Na <hezron.w12@gmail.com>
2024-03-01 16:40:03 +0300

    Committed 2024 0Fr.164003

--- A_app.py
+++ A_app.py
@@ -2,7 +2,7 @@ from gradientai import ExtractParamsSchemaValueType, Gradient
 
 
 
-schema = '{ "131313" { "type": ExtractParamsSchemaValueType.NUMBER, "required": True, }, "ddfsf" { "type": ExtractParamsSchemaValueType.STRING, "required": True, }, "dfg" : { "type": ExtractParamsSchemaValueType.STRING, "required": True, }, }'
+schema = '{ "131313" : { "type": ExtractParamsSchemaValueType.NUMBER, "required": True, }, "ddfsf" { "type": ExtractParamsSchemaValueType.STRING, "required": True, }, "dfg" : { "type": ExtractParamsSchemaValueType.STRING, "required": True, }, }'
 
 dictionary = eval(schema)
 print(dictionary)
