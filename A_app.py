f4ba7b26f52d87f227086c1431f3176510d6d3b8
Hezron Na <hezron.w12@gmail.com>
2024-03-01 16:28:35 +0300

    Committed 2024 0Fr.162835

--- A_app.py
+++ A_app.py
@@ -1,16 +1,1 @@
-import tkinter as tk
-
-def get_button_text():
-    button_text = button.cget("text")
-    print("Button Text:", button_text)
-
-# Create the main window
-root = tk.Tk()
-root.title("Get Button Text Example")
-
-# Create a Button widget
-button = tk.Button(root, text="Click Me", command=get_button_text)
-button.pack(padx=20, pady=20)
-
-# Run the Tkinter event loop
-root.mainloop()
+m = '{131313{ "type": ExtractParamsSchemaValueType.NUMBER, "required": True, }, ddfsf{ "type": ExtractParamsSchemaValueType.STRING, "required": True, }, dfg{ "type": ExtractParamsSchemaValueType.BOOLEAN, "required": True, }, }'
