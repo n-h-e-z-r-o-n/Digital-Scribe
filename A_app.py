a362ad4b3d6dafa025dbf31108fc01986c44498d
Hezron Na <hezron.w12@gmail.com>
2024-03-01 16:30:50 +0300

    Committed 2024 0Fr.163050

--- A_app.py
+++ A_app.py
@@ -1,5 +1,5 @@
-schema = '{131313{ "type": ExtractParamsSchemaValueType.NUMBER, "required": True, }, ddfsf{ "type": ExtractParamsSchemaValueType.STRING, "required": True, }, dfg{ "type": ExtractParamsSchemaValueType.BOOLEAN, "required": True, }, }'
+schema = '{131313 { "type": ExtractParamsSchemaValueType.NUMBER, "required": True, }, ddfsf{ "type": ExtractParamsSchemaValueType.STRING, "required": True, }, dfg{ "type": ExtractParamsSchemaValueType.BOOLEAN, "required": True, }, }'
 
-dictionary = dict(schema)
+dictionary = eval(schema)
 print(dictionary)
 
