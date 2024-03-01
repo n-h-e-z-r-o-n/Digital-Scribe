e6c3bbbc12d78c13afd5add74c3033421a10c265
Hezron Na <hezron.w12@gmail.com>
2024-03-01 11:36:01 +0300

    Committed 2024 0Fr.113601

--- MAIN.py
+++ MAIN.py
@@ -509,8 +509,8 @@ def chat(widget):
         new_entity = tk.Frame(widget, bg='blue', relief=tk.SUNKEN, borderwidth=2, border=1, height=90, width=int(screen_width*0.9747*0.21 -5))
         new_entity.pack(side=tk.BOTTOM, fill=tk.X)
 
-        tk.Label(new_entity,  text="Field Name", bg='green', borderwidth=0, border=0).place(relx=0, rely=0, relwidth=0.25, relheight=0.25)
-
+        tk.Label(new_entity,  text="Field Name", bg='green', borderwidth=0, border=0).place(relx=0, rely=0, relwidth=0.5, relheight=0.3)
+        tk.Label(new_entity, text="Type", bg='green', borderwidth=0, border=0).place(relx=0.5, rely=0, relwidth=0.2, relheight=0.3)
 
 
 
