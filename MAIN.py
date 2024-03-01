693e62a4a17d2e56de39196315b72f3e6bb80085
Hezron Na <hezron.w12@gmail.com>
2024-03-01 16:44:14 +0300

    Committed 2024 0Fr.164414

--- MAIN.py
+++ MAIN.py
@@ -40,11 +40,11 @@ keys = None
 def Entity_Extraction(document, entity_list):
     schema = '{'
     for i in entity_list:
-        schema +=  i[1].get() + '{ "type": ExtractParamsSchemaValueType.' + str(i[2].cget("text")) + ', "required": True, }, '
+        schema +=  i[1].get() + ': { "type": ExtractParamsSchemaValueType.' + str(i[2].cget("text")) + ', "required": True, }, '
         print(i[1].get(), '--', i[2].cget("text"))
     schema += '}'
     print(schema)
-    dictionary = dict(schema)
+    dictionary = eval(schema)
     print(dictionary)
 
     """
