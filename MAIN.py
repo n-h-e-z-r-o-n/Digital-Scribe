811af4d25028446f37a35ee975e9a05622bafec6
Hezron Na <hezron.w12@gmail.com>
2024-03-02 23:30:42 +0300

    Committed 2024 0Sa.233042

--- MAIN.py
+++ MAIN.py
@@ -783,14 +783,14 @@ def conversation(widget):
     conversation_widget = tk.Frame(widget, bg=bg_color, borderwidth=0, border=0)
     conversation_widget.place(relheight=0.96, relwidth=0.9747, rely=0.02, relx=0.0253)
 
-    t1 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=5)
+    t1 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=3)
     t1.place(relheight=0.60, relwidth=0.485, rely=0.03, relx=0.01)
 
-    t2 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=5)
+    t2 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=3)
     t2.place(relheight=0.60, relwidth=0.485, rely=0.03, relx=0.505)
 
     t2 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=1)
-    t2.place(relheight=0.05, relwidth=0.6, rely=0.7, relx=0.01)
+    t2.place(relheight=0.05, relwidth=0.9, rely=0.7, relx=0.01)
 
 
 
