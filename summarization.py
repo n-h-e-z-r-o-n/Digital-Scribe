f5958452c3c3dd4c421f8f9fcbeee9671b95dbdc
Hezron Na <hezron.w12@gmail.com>
2024-03-02 20:11:00 +0300

    Committed 2024 0Sa.201059

--- summarization.py
+++ summarization.py
@@ -9,21 +9,37 @@ os.environ['GRADIENT_WORKSPACE_ID'] = "345ce93a-40e9-4940-aa2e-fa76f1668fcd_work
 mygradient = Gradient()
 
 document = (
-    "In the days ahead of the Vision Pro's launch, Apple has heavily "
-    + "promoted some of the apps destined for its spatial computing "
-    + "headset. Download Disney Plus and watch movies from Tatooine! "
-    + "Slack and Fantastical and Microsoft Office on your face! FaceTime "
-    + "with your friends as a floating hologram! But it's increasingly "
-    + "clear that the early success of the Vision Pro, and much of the "
-    + "answer to the question of what this headset is actually for, will "
-    + "come from a single app: Safari.\n\nThat's right, friends. Web "
-    + "browsers are back. And Apple needs them more than ever if it wants "
-    + "this $3,500 face computer to be a hit. Embracing the web will mean "
-    + "threatening the very things that have made Apple so powerful and "
-    + "so rich in the mobile era, but at least at first, the open web is "
-    + "Apple's best chance to make its headset a winner. Because at least "
-    + "so far, it seems developers are not exactly jumping to build new "
-    + "apps for Apple's new platform."
+""" Good morning, Doctor. I hope you're doing well today.
+
+ Good morning! Yes, I'm doing well, thank you. How about yourself?
+
+ Not too bad, thank you. I've been having some persistent headaches lately, and I thought it would be best to come in and get them checked out.
+
+ I'm glad you came in. Headaches can be quite concerning. Can you tell me more about when they started and how often you've been experiencing them?
+
+ Sure. They started about two weeks ago, and they seem to come and go throughout the day. Sometimes they're dull and achy, and other times they're more sharp and intense.
+
+ Have you noticed any specific triggers or patterns associated with the headaches?
+
+ Well, I've noticed that they tend to worsen when I'm stressed or when I haven't had enough sleep. But other than that, I haven't been able to identify any specific triggers.
+
+ Okay, that's helpful to know. Have you experienced any other symptoms along with the headaches, such as nausea, dizziness, or changes in vision?
+
+ No, not really. Just the headaches themselves.
+
+ Alright. And have you tried taking any over-the-counter medications for the headaches? If so, did they provide any relief?
+
+ Yes, I've tried taking ibuprofen a few times, but it only seems to provide temporary relief. The headaches always seem to come back.
+
+ I see. Well, based on what you've described, it's possible that these headaches could be tension headaches or migraines. However, I'd like to conduct a thorough examination and possibly some tests to rule out any other underlying causes.
+
+ That sounds like a good idea. I just want to make sure there's nothing serious going on.
+
+ Of course. Let's start by checking your blood pressure and conducting a neurological examination. Depending on the results, we may need to consider further imaging studies or blood tests.
+
+ Sounds good. Thank you, Doctor, for taking the time to listen to me and help figure this out.
+
+ You're welcome. It's my job to ensure your health and well-being. Let's work together to get to the bottom of these headaches and find the best course of action moving forward."""
 )
 examples = [
     {
@@ -50,13 +66,12 @@ examples = [
     },
 ]
 
-"""
 result_from_examples = mygradient.summarize(
     document=document,
-    examples=examples,
+    # examples=examples,
 )
-"""
+
 length = SummarizeParamsLength.MEDIUM
 result_from_length = mygradient.summarize(document=document, length=length)
 
-print(result_from_length)
+print(result_from_examples)
