73f5b6d3f0f8973c2af12b59f5f56d980c810ba4
Hezron Na <hezron.w12@gmail.com>
2024-03-02 21:07:36 +0300

    Committed 2024 0Sa.210735

--- MAIN.py
+++ MAIN.py
@@ -72,6 +72,7 @@ def Entity_Extraction(document, entity_list, widget):
 
 
 def D_Summary(document, widget):
+    document = document.get("1.0", "end")
     document = (document.strip())
     gradient = Gradient()
     print(document)
@@ -605,7 +606,7 @@ def chat(widget):
     Extract_entities.place(relheight=0.02, relwidth=0.21, rely=0.751, relx=0.78)
     change_fg_OnHover(Extract_entities, 'red', fg_color)
 
-    Summary_wid = tk.Button(chatbot_widget, text='Summary', fg=fg_color, font=("Bauhaus 93", 10), activebackground=bg_color, bg=bg_color, borderwidth=0, border=0, command=lambda: D_Summary(t1.get("1.0", "end"), t2))
+    Summary_wid = tk.Button(chatbot_widget, text='Summary', fg=fg_color, font=("Bauhaus 93", 10), activebackground=bg_color, bg=bg_color, borderwidth=0, border=0, command=lambda: D_Summary(t1, t2))
     Summary_wid.place(relheight=0.02, relwidth=0.21, rely=0.772, relx=0.78)
     change_fg_OnHover(Summary_wid, 'red', fg_color)
 
