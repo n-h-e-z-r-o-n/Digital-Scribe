693b9f646eee1ebe142671ab4aef5d638a7ed47f
Hezron Na <hezron.w12@gmail.com>
2024-03-02 23:07:45 +0300

    Committed 2024 0Sa.230745

--- MAIN.py
+++ MAIN.py
@@ -786,6 +786,8 @@ def conversation(widget):
     t1 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=5)
     t1.place(relheight=0.60, relwidth=0.755, rely=0.03, relx=0.0253)
 
+    t2 = tk.Text(conversation_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=5)
+    t2.place(relheight=0.60, relwidth=0.755, rely=0.03, relx=0.0253)
 
 
 
