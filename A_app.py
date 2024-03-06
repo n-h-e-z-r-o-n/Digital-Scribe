679ad433bbbcc61111270fb1c1dcc0de185785da
Hezron Na <hezron.w12@gmail.com>
2024-03-06 12:43:45 +0300

    Committed 2024 0We.124345

--- A_app.py
+++ A_app.py
@@ -13,8 +13,8 @@ data = ''
 for paragraph in document.paragraphs:
     print(paragraph.text)
     print(paragraph.style.name)
-    if paragraph.style.name == 'Normal':
-        data += paragraph.text
+    if paragraph.style.name == 'List Paragraph':
+        print( '\t •', paragraph.text)
 print(data)
 
 
