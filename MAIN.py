9d9dd8f08415b51beb30e04facd8cd4b295ab684
Hezron Na <hezron.w12@gmail.com>
2024-03-06 15:56:12 +0300

    Committed 2024 0We.155612

--- MAIN.py
+++ MAIN.py
@@ -1075,8 +1075,8 @@ def settings(widget):
     g2.place(relheight=0.4, relwidth=0.41, rely=0.5, relx=0.0253)
 
     tk.Label(g2, text="PERSONALIZATION ", bg=bg_color, fg=fg_color, font=("Georgia", 12, 'bold'), anchor='w', borderwidth=0, border=0).place(relheight=0.07, relwidth=0.6, rely=0, relx=0)
-    tk.Label(g2, text="Theme :", bg=bg_color, fg=fg_color, font=("Calibri", 10, 'bold'), anchor='w', borderwidth=0, border=0).place(relheight=0.07, relwidth=0.24, rely=0.071, relx=0)
-    themes_change = tk.Button(g2, text = 'white', bg=bg_color, fg=fg_color, borderwidth=0, border=1, font=("Courier New", 10))
+    tk.Label(g2, text="current theme :", bg=bg_color, fg=fg_color, font=("Calibri", 10, 'bold'), anchor='w', borderwidth=0, border=0).place(relheight=0.07, relwidth=0.24, rely=0.071, relx=0)
+    themes_change = tk.Button(g2, text = 'window(dark)', bg=bg_color, fg=fg_color, borderwidth=0, border=1, font=("Courier New", 10))
     themes_change.place(relheight=0.07, relwidth=0.3, rely=0.071, relx=0.25)
     change_fg_OnHover(themes_change, fg_hovercolor, fg_color)
     
