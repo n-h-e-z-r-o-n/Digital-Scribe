02ff955bbf9e88fa4578f8bfdf120a75a38d75c3
Hezron Na <hezron.w12@gmail.com>
2024-03-08 20:36:10 +0300

    Committed 2024 0Fr.203610

--- MAIN.py
+++ MAIN.py
@@ -1233,14 +1233,14 @@ def chat_me(widget):
         print("- ", num_lines, " - ", current)
 
 
-        if num_lines > current:
+        if num_lines >= current:
             num_y = num_y - 0.02
             num_height = num_height + 0.02
             widget.forget()
             widget.place(relheight=num_height, relwidth=0.6, rely=num_y, relx=0.2)
             current = current + 130
 
-        elif num_lines < current and num_lines < 130:
+        elif (num_lines < current) and (num_lines > 130):
             num_y = num_y + 0.02
             num_height = num_height - 0.02
             widget.forget()
