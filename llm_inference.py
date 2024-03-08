d1d7603776bb27e0307760faf912411f203648b8
Hezron Na <hezron.w12@gmail.com>
2024-03-08 17:51:18 +0300

    Committed 2024 0Fr.175118

--- llm_inference.py
+++ llm_inference.py
@@ -7,17 +7,17 @@ from langchain_community.llms import GradientLLM
 from langchain.prompts import PromptTemplate
 import gradientai
 import os
-
+from gradientai import Gradient
 
 os.environ['GRADIENT_ACCESS_TOKEN'] = "MU96F09nGNZC8R1B3d4XfbKqgyKrfqIs"
 os.environ['GRADIENT_WORKSPACE_ID'] = "1b99bbdd-1360-4321-a152-fc8822334cd0_workspace"
 
 fine_tuned_Model_Id = "d189f721-ae17-4545-a0ad-f95194e857f5_model_adapter"  #  initializes a GradientLLM with our fine-tuned model by specifying our model ID.
 
-print(dir(GradientLLM))
+gradient =  Gradient()
 
-help(GradientLLM)
-
+base_model = gradient.get_base_model(base_model_slug="nous-hermes2")
+print(base_model.id)
 
 llm = GradientLLM(
     model="99148c6d-c2a0-4fbe-a4a7-e7c05bdb8a09_base_ml_model",
