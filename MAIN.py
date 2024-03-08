06e9c454690d0f2a3ea2ae077fc4438e1157a9e9
Hezron Na <hezron.w12@gmail.com>
2024-03-08 19:23:03 +0300

    Committed 2024 0Fr.192302

--- MAIN.py
+++ MAIN.py
@@ -289,11 +289,11 @@ def Chat_bot_inference(Question, widget1, widget2):
         llm_inference_initializ()
 
     widget2.config(state=tk.NORMAL)
-    widget2.insert(tk.END, f"YOU\n{Question}\n")
+    widget2.insert(tk.END, f"🆈🅾🆄\n{Question}\n\n")
 
     Answer = llm_chain.invoke(input=f"\n{Question}")
 
-    widget2.insert(tk.END, f"⊙ BOT{Answer['text']}\n")
+    widget2.insert(tk.END, f"🅱🅾🆃\n{Answer['text']}\n")
 
     widget2.config(state=tk.DISABLED)
     widget1.forget()
