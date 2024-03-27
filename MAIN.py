0cc89a113d0ea5eefd095881648427c5065778d9
Hezron Na <hezron.w12@gmail.com>
2024-03-27 23:07:58 +0300

    Committed 2024 0We.230758

--- MAIN.py
+++ MAIN.py
@@ -1399,7 +1399,7 @@ def User_Home_page(widget):
     container2 = tk.Frame(Home_page_frame, bg=bg_color)
     container2.place(rely=0.02, relx=0.0253, width=int(screen_width * 0.9747), height=int(screen_height * 0.96) )  #place(relheight=0.96, relwidth=0.9747, rely=0.02, relx=0.0253, )
 
-    """
+
     nav_bar_color = bg_color
     nav_bar_btn_hover_color = bg_hovercolor
     nav_bar = tk.Frame(Home_page_frame, bg=nav_bar_color)
@@ -1485,7 +1485,7 @@ def User_Home_page(widget):
 
     return Home_page_frame
 
-    """
+
 
 def Welcome_Page(wiget):
     global screen_width, screen_height
