31a147d75f94a0d17208bfedd3fb9051089a5c4c
Hezron Na <hezron.w12@gmail.com>
2024-03-27 22:50:43 +0300

    Committed 2024 0We.225042

--- MAIN.py
+++ MAIN.py
@@ -1394,6 +1394,7 @@ def User_Home_page(widget):
     Home_page_frame.pack(fill=tk.BOTH, expand=True)
 
     container1 = tk.Frame(Home_page_frame, bg=bg_color)
+    container1.place(rely=0.02, relx=0, width=int(screen_width * 0.025), height=int(screen_height * 0.96)
 
     container2 = tk.Frame(Home_page_frame, bg=bg_color)
 
