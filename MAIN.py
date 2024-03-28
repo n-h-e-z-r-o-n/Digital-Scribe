ef0dce509944d6920c96fb2105790fe6786b07e8
Hezron Na <hezron.w12@gmail.com>
2024-03-28 15:02:16 +0300

    Committed 2024 0Th.150216

--- MAIN.py
+++ MAIN.py
@@ -817,9 +817,9 @@ def change_color(widget, button):
             wdget.config(bg=bg_color)
         elif isinstance(wdget, tk.Checkbutton):
             wdget.config(bg=bg_color, activebackground=bg_color)
-       
-        PanedWindow
-
+        elif isinstance(wdget, tk.PanedWindow):
+            wdget.config(bg=bg_color)
+            
         else:
             # widget.config(bg=bg_icolor, fg='white')
             pass
