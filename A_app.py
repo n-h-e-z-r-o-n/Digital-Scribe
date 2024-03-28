963030e18a2dd37e174f7ba4ef257894b94ecfdf
Hezron Na <hezron.w12@gmail.com>
2024-03-28 14:31:09 +0300

    Committed 2024 0Th.143109

--- A_app.py
+++ A_app.py
@@ -9,7 +9,7 @@ root.title("Resizable Widget Demo")
 
 # Create a PanedWindow
 
-F = tk.Frame(root)
+F = tk.Frame(root, bg='black')
 F.place(relheight=0.5, relwidth=0.5, relx=0.1, rely=0.1)
 paned_window = tk.PanedWindow(F, orient=tk.VERTICAL, sashwidth=8, sashrelief=tk.RAISED)
 paned_window.pack(fill=tk.BOTH, expand=True)
