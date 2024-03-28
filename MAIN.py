58debc98176874e355b396ace672a2f8a341c20a
Hezron Na <hezron.w12@gmail.com>
2024-03-28 14:37:21 +0300

    Committed 2024 0Th.143721

--- MAIN.py
+++ MAIN.py
@@ -905,12 +905,12 @@ def chat(widget):
     font_size_entry.insert(0, defalt_font_size)
 
     # ======================================================================================================================================================================================================
-
+    
     t1 = tk.Text(chatbot_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=2, border=5)
-    t1.place(relheight=0.70, relwidth=0.75, rely=0.03, relx=0.0253)
+    #t1.place(relheight=0.70, relwidth=0.75, rely=0.03, relx=0.0253)
 
     t2 = tk.Text(chatbot_widget, bg=bg_color, fg=fg_color, relief=tk.SUNKEN, font=("Times New Roman", 13), borderwidth=4, border=1)
-    t2.place(relheight=0.25, relwidth=0.75, rely=0.74, relx=0.0253)
+    #t2.place(relheight=0.25, relwidth=0.75, rely=0.74, relx=0.0253)
     t2.config(state=tk.DISABLED)
 
     threading.Thread(target=font_change, args=(font_style_entry, font_size_entry, t1,)).start()
