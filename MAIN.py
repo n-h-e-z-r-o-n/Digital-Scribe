e5fc2968b8412a35ba0153b325bdac22a1d68770
Hezron Na <hezron.w12@gmail.com>
2024-03-29 12:09:07 +0300

    Committed 2024 0Fr.120907

--- MAIN.py
+++ MAIN.py
@@ -376,6 +376,8 @@ def on_mouse_wheel(widget, event):  # Function to handle mouse wheel scrolling
 def on_frame_configure(widget, event):  # Update the canvas scrolling region when the large frame changes size
     widget.configure(scrollregion=widget.bbox("all"))
 
+    
+
 def widget_scroll_bind(widget):
     widget.bind("<Configure>", lambda e: on_frame_configure(widget, e))
     widget.bind("<MouseWheel>", lambda e: on_mouse_wheel(widget, e))
