5b1a28c863d3f5472f7e59dd838b776589755a65
Hezron Na <hezron.w12@gmail.com>
2024-03-29 13:27:46 +0300

    Committed 2024 0Fr.132746

--- Real-Time Transcription.py
+++ Real-Time Transcription.py
@@ -74,7 +74,7 @@ def RUN_OFFLINE_speech_recognition():
             # cased = subprocess.check_output('python recasepunc/recasepunc.py predict recasepunc/checkpoint', shell=True, text=True, input=text)
             # output.append_stdout(cased)
             # time.sleep(1)
-        print("trrrrrr")
+        
 
 
     start_recording()
