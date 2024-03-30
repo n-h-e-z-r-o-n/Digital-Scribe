fa3c6771d07dae1ceed209981b93fcc9d5cac17c
Hezron Na <hezron.w12@gmail.com>
2024-03-30 23:01:42 +0300

    Committed 2024 0Sa.230142

--- test.py
+++ test.py
@@ -14,21 +14,15 @@ os.environ['GRADIENT_WORKSPACE_ID'] = "1b99bbdd-1360-4321-a152-fc8822334cd0_work
 
 fine_tuned_Model_Id = "d189f721-ae17-4545-a0ad-f95194e857f5_model_adapter"  #  initializes a GradientLLM with our fine-tuned model by specifying our model ID.
 
-gradient =  Gradient()
+gradient = Gradient()
 
 base_model = gradient.get_base_model(base_model_slug="nous-hermes2")
 print(base_model.id)
-
 
 llm = GradientLLM(
     model=base_model.id,
     model_kwargs=dict(max_generated_token_count=510),
 )
-
-
-
-
-#template = """### Instruction: {Instruction} \n\n### Response:"""
 
 template = """ formats the conversation with correct grammar.
 conversation: {conversation}
@@ -38,13 +32,7 @@ prompt = PromptTemplate(template=template, input_variables=["conversation"])
 
 llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
-#Question = "What diseases are prevelant in dairy small ruminant, and what managment practice can mitigate their impact "
-
-#Answer = llm_chain.invoke(input=f"{Question}")
-#print(Answer['text'])
-
 while True:
     Question = input()
-
     Answer = llm_chain.invoke(input=f"{Question}")
     print(Answer['text'])
