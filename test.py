a2b7e289cd8c11226fab196e629a6cb4a42079b0
Hezron Na <hezron.w12@gmail.com>
2024-03-30 22:53:25 +0300

    Committed 2024 0Sa.225325

--- test.py
+++ test.py
@@ -31,14 +31,14 @@ llm = GradientLLM(
 #template = """### Instruction: {Instruction} \n\n### Response:"""
 
 template = """You are a AI that formats a conversation with correct grammar.
-conversation: {Instruction}
+conversation: {conversation}
 Chatbot:"""
 
-prompt = PromptTemplate(template=template, input_variables=["Instruction"])
+prompt = PromptTemplate(template=template, input_variables=["conversation"])
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 
-llm_chain = LLMChain(prompt=prompt, llm=llm)
+llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
 #Question = "What diseases are prevelant in dairy small ruminant, and what managment practice can mitigate their impact "
 
