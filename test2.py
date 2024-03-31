dd24ca1e90df52fe7bb63c5b60b20fa2633419cc
Hezron Na <hezron.w12@gmail.com>
2024-03-31 12:06:01 +0300

    Committed 2024 0Su.120601

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 def get_text_range():
     start_index = "1.0"  # Start from the beginning of the text widget
-    end_index = "3.0"    # End at line 3
+    end_index = "7.0"    # End at line 3
     text_range = text_widget.get(start_index, end_index)
     print("Text in the range:", text_range)
 
