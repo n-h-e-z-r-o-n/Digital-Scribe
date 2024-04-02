4a33f6746d8b84089e74a1d485a3bd670fea1b7a
Hezron Na <hezron.w12@gmail.com>
2024-04-02 14:01:09 +0300

    Committed 2024 0Tu.140109

--- test2.py
+++ test2.py
@@ -1,5 +1,12 @@
 from tkinter import filedialog
+import whisper
+
+
 filetypes = [("Audio Files", "*.mp3;*.wav;*.ogg;*.flac;*.aac")]
 file_path = filedialog.askopenfilename(filetypes=filetypes)
+model = whisper.load_model("base")
+print(filetypes)
+result = model.transcribe("file_path")
+print(result["text"])
 
 help(file_path)
