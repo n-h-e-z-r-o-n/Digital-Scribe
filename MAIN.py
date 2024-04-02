f6b00d8ab6ba86246c43a9fc8e5aedf4ec5a063b
Hezron Na <hezron.w12@gmail.com>
2024-04-02 21:02:21 +0300

    Committed 2024 0Tu.210221

--- MAIN.py
+++ MAIN.py
@@ -45,7 +45,7 @@ import pyaudio
 import json
 from vosk import Model, KaldiRecognizer
 import whisper
-
+import wave
 # =============================== Global variable decoration  ============================================================================================
 root = None
 screen_width: int
@@ -768,6 +768,21 @@ def upload_audio_file(widget, bt_widget):
 
     threading.Thread(target=run).start()
 
+def download_transcribed_audio(widget):
+        global audio_frames
+        channels = 1  # Mono
+        sample_width = 2  # 16-bit audio
+        sample_rate = 16000  # Sample rate (Hz)
+        output_file = 'wisper_model_tiny.wav'
+        # Open the output file in write mode
+        with wave.open(output_file, 'wb') as output_wave:
+            # Set audio parameters
+            output_wave.setnchannels(channels)
+            output_wave.setsampwidth(sample_width)
+            output_wave.setframerate(sample_rate)
+
+            # Write the audio frames to the file
+            output_wave.writeframes(b''.join(audio_frames))
 # =============================== scroll Functions definition ===============================================================================================================
 
 
