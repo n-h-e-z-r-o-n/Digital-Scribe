e2c5d132278340004671f6670fb949a873632a4a
Hezron Na <hezron.w12@gmail.com>
2024-04-03 20:54:27 +0300

    Committed 2024 0We.205427

--- A_app.py
+++ A_app.py
@@ -1,33 +1,18 @@
-def integrate_strings(original, edited):
-    original_list =  original.split()
-    edited_list = edited.split()
+def integrate_strings(old , edited, new):
+    old = old.split()
+    edited = edited.split()
+    new = new.split()
 
-    original_len = len(original_list)
-    edited_len = len(edited_list)
-    integreted = ""
-    print(original_list)
-    print(edited_list)
+    for i in new:
+        
+        if old[i] == edited[i]:
+            old[i] = new[i]
 
-
-    count = 0
-    o_word_index = 0
-    while count <= edited_len-1:
-        o_word = original_list[o_word_index]
-        e_word = edited_list[count]
-        if o_word == e_word:
-            integreted += o_word + " "
-            o_word_index += 1
-        else:
-            integreted += e_word + " "
+    integreted = " ".join(old)
 
 
 
-        count += 1
 
-    print(original_list[o_word_index:])
-    for i in original_list[count:]:
-        integreted += i
-    print(count)
     return integreted
 
 
