678d001c286b5b7ad6f4577903101f9ac3edc602
Hezron Na <hezron.w12@gmail.com>
2024-04-03 17:32:52 +0300

    Committed 2024 0We.173252

--- A_app.py
+++ A_app.py
@@ -10,30 +10,14 @@ def integrate_strings(original, edited):
     count = 0
     for i in original_list:
         if i == edited_list[count]:
-
+            integreted += i + ' '
         count += 1
 
+    return integreted
 
-
-    """
-    integrated_string = ""
-    # Iterate through the characters of both strings simultaneously
-    for char1, char2 in zip(string1, string2):
-        # If characters are different, append both to the integrated string
-        if char1 != char2:
-            integrated_string += f"[{char1}/{char2}]"
-        else:
-            integrated_string += char1
-    # If one string is longer than the other, append the remaining characters
-    if len(string1) > len(string2):
-        integrated_string += string1[len(string2):]
-    elif len(string2) > len(string1):
-        integrated_string += string2[len(string1):]
-    return integrated_string
-    """
 
 # Example usage:
-string1 = "hello world today."
-string2 = "hallo welt today."
-integrated = integrate_strings(string1, string2)
+original = "hello world today."
+edited = "hallo welt today."
+integrated = integrate_strings(original, string2)
 print(integrated)
