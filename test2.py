0a9b4c4accc84ee5135f457878ea432beb0aea6d
Hezron Na <hezron.w12@gmail.com>
2024-04-03 15:22:04 +0300

    Committed 2024 0We.152204

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 def highlight_words(widget):
-    widget.tag_configure("highlight", background="yellow")  # Configure a tag for highlighting
+    widget.tag_configure("highlight", foreground="yellow")  # Configure a tag for highlighting
 
     words_to_highlight = ["Python", "Tkinter"]  # List of words to highlight
 
