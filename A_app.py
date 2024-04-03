cae70c4de86f883aca780493004a450456663856
Hezron Na <hezron.w12@gmail.com>
2024-04-03 21:13:42 +0300

    Committed 2024 0We.211342

--- A_app.py
+++ A_app.py
@@ -2,14 +2,11 @@ def integrate_strings(old , edited, new):
     old = old.split()
     edited = edited.split()
     new = new.split()
-    index = 0
-    for i in old:
-        if i == new[index]:
+    print(len(old))
+    pos = len(old)-1
+    data = new[pos:]
 
-            old[index] = new[index]
-        index += 1
-
-    return integreted
+    return data
 
 
 # Example usage:
@@ -18,7 +15,7 @@ old = "hallo world boy today hezron"
 edited = "hallo welt boy today hezron."
 new = "hello world today hezron. sample string containing words."
 
-
+pri
 integrated = integrate_strings(old , edited, new)
 print("-------------------------")
 print(integrated)
