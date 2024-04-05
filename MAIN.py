a990ecfe77e195ad5e3311e07708c941fb5dcacf
Hezron Na <hezron.w12@gmail.com>
2024-04-05 23:34:55 +0300

    Committed 2024 0Fr.233455

--- MAIN.py
+++ MAIN.py
@@ -656,7 +656,7 @@ def RUN_OFFLINE_speech_recognition(widget, widget1=None, widget2=None, Record_bt
                     widget.see(tk.END)
                     if pos == 10:
                         transcribe_audio(audio_frames, widget1)
-                        #Entity_Extraction(widget1)
+                        Entity_Extraction(widget1)
                         D_Summary(widget1, widget2)
                         widget.delete(1.0, tk.END)
                         widget2.insert(tk.END, Recording_entity + Recording_summary)
