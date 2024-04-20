c950141667016ab4441f6676dece59537fc249c2
Hezron Na <hezron.w12@gmail.com>
2024-04-20 21:31:00 +0300

    Committed 2024 0Sa.213100

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ def modify_css(css_file_path):
     # Read the content of the CSS file
     bg_color = 'blue'
     fg_color = 'red'
-    css_style = " /* Your CSS styles go here */ \n" + "body {\nbackground-color:" + f"{bg_color};\n color: {white}\n " + "}"
+    css_style = " /* Your CSS styles go here */ \n\n" + "body {\n\tbackground-color:" + f"{bg_color};\n\tcolor: {fg_color}\n;" + "}"
 
 
 
