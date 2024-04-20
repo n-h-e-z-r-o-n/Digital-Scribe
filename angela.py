33e645e4ad672071abcd1eb77767ba8afb95f37b
Hezron Na <hezron.w12@gmail.com>
2024-04-20 16:57:25 +0300

    Committed 2024 0Sa.165725

--- angela.py
+++ angela.py
@@ -1,22 +1,4 @@
-import requests
+import http.client
 
-response = requests.post(
-    f"https://api.stability.ai/v2beta/stable-image/generate/core",
-    headers={
-        "authorization": f"Bearer sk-wEBf1qTVCMFuZ8tH7J171b4zCdcHIRtKUcGJiNER3pqWavHR",
-        "accept": "image/*"
-    },
-    files={
-        "none": ''
-    },
-    data={
-        "prompt": "dog wearing black glasses",
-        "output_format": "webp",
-    },
-)
-
-if response.status_code == 200:
-    with open("./lighthouse.webp", 'wb') as file:
-        file.write(response.content)
-else:
-    raise Exception(str(response.json()))
+# Create an HTTPS connection to example.com
+conn = http.client.HTTPSConnection("file:///C:\Users\HEZRON WEKESA\Desktop\python Project\Mental_health Ai\Mental_App\html\index.html")
