09a85eb933eb7aa36ae3a5fdd9f80d03c5eef3e1
Hezron Na <hezron.w12@gmail.com>
2024-04-20 15:34:40 +0300

    Committed 2024 0Sa.153440

--- html/o.py
+++ html/o.py
@@ -1,16 +1,39 @@
-from flask import Flask, render_template, request
+#!C:\Users\HEZRON WEKESA\AppData\Local\Programs\Python\Python310\python.exe
+print("Content-Type: text/html")
+print()
+print('<html>')
+print('<body>')
 
-app = Flask(__name__)
+import cgi,cgitb
+cgitb.enable()
+form = cgi.FieldStorage()
+#receive  values from user form and trim white spaces
+guest_name = form.getvalue('guest_name')
+guest_phone_number = form.getvalue('guest_phone_number')
+guest_email = form.getvalue('guest_email')
+guest_room_type = form.getvalue('guest_room_type')
 
-@app.route('/')
-def index():
-    return render_template('./index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = request.form['data']
-    # Process the data as needed
-    return "Received: " + data
+import json
+# Data to be written
+dictionary ={
+    "Guest_name" : guest_name,
+    "Guest_Phone" : guest_phone_number,
+    "Guest_Email" : guest_email,
+    "Guest_room" : guest_room_type
+}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+with open('guest_detail.json') as json_file:
+    data = json.load(json_file)
+
+temp = data['Guests']
+temp.append(dictionary)
+with open('guest_detail.json','w') as json_file_write:
+   json.dump(data,json_file_write, indent=4)
+
+print('<h1 style="text-align: center; display: flex; flex-direction: column; justify-content: center;"> You Have Been Added Successfuly in The Booking List</h1>')
+
+print('</body>')
+
+print('</html>')
