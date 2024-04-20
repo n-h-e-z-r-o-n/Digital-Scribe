a67faeda3e21b0f07f72eef1e12091dd4a760b20
Hezron Na <hezron.w12@gmail.com>
2024-04-20 16:13:55 +0300

    Committed 2024 0Sa.161354

--- html/script.py
+++ html/script.py
@@ -1,20 +1,23 @@
 #!C:\Users\HEZRON WEKESA\AppData\Local\Programs\Python\Python310\python.exe
-import cgi, cgitb
+print("Content-Type: text/html")
+print()
+print('<html>')
+print('<body>')
 
+import cgi
+import cgitb
+cgitb.enable()
 form = cgi.FieldStorage()
+#receive  values from user form and trim white spaces
+guest_name = form.getvalue('guest_email')
+guest_phone_number = form.getvalue('guest_password')
 
-username = form["username"].value
-emailaddress = form["emailaddress"].value
+print(guest_name, "\n", guest_phone_number)
 
-print("Content-type:text/html\r\n\r\n")
 
-print("<html>")
-print("<head>")
-print("<title> MY FIRST CGI FILE </title>")
-print("</head>")
-print("<body>")
-print("<h3> This is HTML's Body Section </h3>")
-print(username)
-print(emailaddress)
-print("</body>")
-print("</html>")
+
+print('<h1 style="text-align: center; display: flex; flex-direction: column; justify-content: center;"> You Have Been Added Successfuly in The Booking List</h1>')
+
+print('</body>')
+
+print('</html>')
