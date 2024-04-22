13e67c7381467e0232869c353dbf3d2bcf97be76
Hezron Na <hezron.w12@gmail.com>
2024-04-22 17:53:03 +0300

    Committed 2024-04-22 17:53:02

--- test3.py
+++ test3.py
@@ -1,5 +1,30 @@
-from docx2pdf import convert
+from fpdf import FPDF
+from PyPDF2 import PdfFileMerger
 
-convert(r"C:\Users\HEZRON WEKESA\Desktop\New Text Document.txt", "output.pdf")
+def create_pdf(input_file):
+    # Create a new FPDF object
+    pdf = FPDF()
 
-import pdfconverter
+    # Open the text file and read its contents
+    with open(input_file, 'r') as f:
+        text = f.read()
+
+    # Add a new page to the PDF
+    pdf.add_page()
+
+    # Set the font and font size
+    pdf.set_font('Arial', size=12)
+
+    # Write the text to the PDF
+    pdf.write(5, text)
+
+    # Save the PDF
+    pdf.output('output.pdf')
+
+    # If a template PDF is specified, merge it with the new PDF
+    merger = PdfFileMerger()
+    template_pdf = 'template.pdf'
+    if template_pdf:
+        merger.append(PdfFileReader(open(template_pdf, 'rb')))
+        merger.append(PdfFileReader(open('output.pdf', 'rb')))
+        merger.write('merged_output.pdf')
