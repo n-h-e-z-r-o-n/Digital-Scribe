64208bce40e9231e7430b7272329be1c3b3863d5
Hezron Na <hezron.w12@gmail.com>
2024-04-23 16:30:49 +0300

    Committed 2024 0Tu.163049

--- test3.py
+++ test3.py
@@ -1,7 +1,11 @@
+imp
 import ctypes
-import tkinter as tk
 from webview.window import Window
 from webview.platforms.edgechromium import EdgeChrome
+#from System import IntPtr, Int32, Func, Type, Environment
+from System.Windows.Forms import Control
+#from System.Threading import ApartmentState, ThreadStart, SynchronizationContext, SendOrPostCallback
+#from System.Threading import Thread as System_Thread
 
 user32 = ctypes.windll.user32
 
@@ -10,31 +14,54 @@ class WebView2(tk.Frame):
     def __init__(self, parent, width: int, height: int, url: str = '', **kw):
         global bg_color
         tk.Frame.__init__(self, parent, width=width, height=height, **kw)
-        self.parent = parent
+        control = Control()
+        uid = 'master'
+        window = Window(uid, str(id(self)), url=None, html=None, js_api=None, width=width, height=height, x=None, y=None,
+                        resizable=True, fullscreen=False, min_size=(200, 100), hidden=False,
+                        frameless=False, easy_drag=True,
+                        minimized=False, on_top=False, confirm_close=False, background_color=bg_color,
+                        transparent=False, text_select=True, localization=None,
+                        zoomable=True, draggable=True, vibrancy=False)
+        self.window = window
+        self.web_view = EdgeChrome(control, window, None)
+        self.control = control
+        self.web = self.web_view.web_view
         self.width = width
         self.height = height
-        self.url = url
+        self.parent = parent
+        self.chwnd = int(str(self.control.Handle))
+        user32.SetParent(self.chwnd, self.winfo_id())
+        user32.MoveWindow(self.chwnd, 0, 0, width, height, True)
+        self.loaded = window.events.loaded
+        self.__go_bind()
+        if url != '':
+            self.load_url(url)
+        self.core = None
+        self.web.CoreWebView2InitializationCompleted += self.__load_core
 
-        self.__create_web_view()
-
-    def __create_web_view(self):
-        self.web_view = EdgeChrome(self, None, None)
-        self.web = self.web_view.web_view
-
+    def __go_bind(self):
         self.bind('<Destroy>', lambda event: self.web.Dispose())
         self.bind('<Configure>', self.__resize_webview)
-
-        self.load_url(self.url)
+        self.newwindow = None
 
     def __resize_webview(self, event):
-        self.web_view.resize(self.winfo_width(), self.winfo_height())
+        user32.MoveWindow(self.chwnd, 0, 0, self.winfo_width(), self.winfo_height(), True)
+
+    def __load_core(self, sender, _):
+        self.core = sender.CoreWebView2
+        self.core.NewWindowRequested -= self.web_view.on_new_window_request
+        # Prevent opening new windows or browsers
+        self.core.NewWindowRequested += lambda _, args: args.Handled(True)
+
+        if self.newwindow != None:
+            self.core.NewWindowRequested += self.newwindow
+        settings = sender.CoreWebView2.Settings  # 设置
+        settings.AreDefaultContextMenusEnabled = False  # 菜单
+        settings.AreDevToolsEnabled = False  # 开发者工具
+        # self.core.DownloadStarting+=self.__download_file
 
     def load_url(self, url):
         self.web_view.load_url(url)
 
     def reload(self):
-        self.web.Reload()
-root = tk.Tk()
-pdf_view_frame = WebView2(root, 500, 500)
-pdf_view_frame.place(relheight=1, relwidth=1, relx=0, rely=0)
-root.mainloop()
+        self.core.Reload()
