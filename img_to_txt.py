92014ef549ef62171a8dbf9343cbad231e7e68f7
Hezron Na <hezron.w12@gmail.com>
2024-04-25 11:16:56 +0300

    Committed 2024 0Th.111656

--- img_to_txt.py
+++ img_to_txt.py
@@ -3,8 +3,8 @@ from PIL import Image
 import pytesseract
 
 # If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
-# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+#pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # Simple image to string
 print(pytesseract.image_to_string(Image.open(r"C:\Users\HEZRON WEKESA\OneDrive\Pictures\AdmissionLetter.png")))
