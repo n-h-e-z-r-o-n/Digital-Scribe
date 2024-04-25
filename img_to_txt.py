dd0f10ddefd7b4cce8910fb25a93ed3f3f6eb1af
Hezron Na <hezron.w12@gmail.com>
2024-04-25 14:46:56 +0300

    Committed 2024 0Th.144656

--- img_to_txt.py
+++ img_to_txt.py
@@ -7,7 +7,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 # Simple image to string
-image_path = r"C:\Users\HEZRON WEKESA\Downloads\after_2-2843848629.JPG"
+image_path = r"C:\Users\HEZRON WEKESA\Downloads\feature2-216988770.jpg"
 print(pytesseract.image_to_string(Image.open(image_path)))
 
 # In order to bypass the image conversions of pytesseract, just use relative or absolute image path
