527d36201ad375c5bfa5d2b1bbe8bd98073a7b21
Hezron Na <hezron.w12@gmail.com>
2024-05-03 14:01:25 +0300

    Committed 2024 0Fr.140125

--- MAIN.py
+++ MAIN.py
@@ -2348,11 +2348,11 @@ def Recodes_Page(widget):
 
                 audio_Lable = tk.Label(audio_wid, text=audio_file, bg="white", font=("Calibri", font_size),  borderwidth=0, border=0)
                 audio_Lable.place(relheight=1, relwidth=0.7, rely=0, relx=0.)
-                audio_play_btn = tk.Label(audio_wid, text="⊵", bg="white", font=("Calibri", font_size), borderwidth=0, border=0)
+                audio_play_btn = tk.Label(audio_wid, text="►", bg="white", font=("Arial Rounded MT Bold", font_size), borderwidth=0, border=0)
                 audio_play_btn.place(relheight=1, relwidth=0.1, rely=0, relx=0.7)
-                audio_download_btn = tk.Label(audio_wid, text="d", bg="white", font=("Calibri", font_size), borderwidth=0, border=0)
+                audio_download_btn = tk.Label(audio_wid, text="⍊", bg="white", font=("Arial Rounded MT Bold", font_size), borderwidth=0, border=0)
                 audio_download_btn.place(relheight=1, relwidth=0.1, rely=0, relx=0.8)
-                audio_push_btn = tk.Label(audio_wid, text="p", bg="white", font=("Calibri", font_size), borderwidth=0, border=0)
+                audio_push_btn = tk.Label(audio_wid, text="⌥", bg="white", font=("Arial Rounded MT Bold", font_size), borderwidth=0, border=0)
                 audio_push_btn.place(relheight=1, relwidth=0.1, rely=0, relx=0.9)
 
                 audio_wid.pack(expand=True, fill=tk.X)  # .place(rel height=0.04, relwidth=1, rely=rely, relx=0)
