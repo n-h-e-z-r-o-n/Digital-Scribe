e16570c989e9690102ec32fac82ed9d2c722f1e7
Hezron Na <hezron.w12@gmail.com>
2024-05-03 11:10:14 +0300

    Committed 2024 0Fr.111014

--- test3.py
+++ test3.py
@@ -14,26 +14,23 @@ Audio_recodes_frame.place(relheight=0.9, relwidth=0.3, rely=0.02, relx=0.02)
 Audio_recodes_canvas = tk.Canvas(Audio_recodes_frame)
 Audio_recodes_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-scrollbar = ttk.Scrollbar(Audio_recodes_frame, orient=tk.VERTICAL, command=on_scroll)
+scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=on_scroll)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
 
 Audio_recodes_canvas.configure(yscrollcommand=scrollbar.set)
 frame = ttk.Frame(Audio_recodes_canvas)
 Audio_recodes_canvas.create_window((0, 0), window=frame, anchor=tk.NW)
 
+Audio_recodes_canvas.bind_all("<MouseWheel>", lambda e: Audio_recodes_canvas.yview_scroll(int(-1 * (e.delta / 120)), "units"))
+
+
+Audio_recodes_canvas.configure(scrollregion=Audio_recodes_canvas.bbox("all"))
+
 
 for i in range(50):
     ttk.Label(frame, text="Label {}".format(i)).pack()
 
-# Bind mousewheel scrolling (optional)
-def on_mousewheel(event):
-    Audio_recodes_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
-
-
-
 # Update canvas scrolling region
 frame.update_idletasks()
-Audio_recodes_canvas.configure(scrollregion=Audio_recodes_canvas.bbox("all"))
 
 root.mainloop()
