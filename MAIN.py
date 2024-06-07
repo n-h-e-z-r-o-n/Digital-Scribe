e198993a506b1e139216b161f942bc3e847ce0da
Hezron Na <hezron.w12@gmail.com>
2024-06-07 12:02:24 +0300

    Committed 2024 0Fr.120224

--- MAIN.py
+++ MAIN.py
@@ -3459,8 +3459,8 @@ def main():
 
     title_bar_color(bg_color)
 
-    User_Home_page(root)
-    # Welcome_Page(root)
+    #User_Home_page(root)
+    Welcome_Page(root)
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
