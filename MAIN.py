08f6aab877d8d3ae4bf3674fd00de94a10526c1c
Hezron Na <hezron.w12@gmail.com>
2024-06-08 17:17:38 +0300

    Committed 2024 0Sa.171738

--- MAIN.py
+++ MAIN.py
@@ -3540,33 +3540,33 @@ def main():
 
     Welcome_Page(root)
 
-    #try:
-    with open('./Data_Raw/CUR_user.json', 'r') as openfile:  # Reading from json file
-        cur_detail = json.load(openfile)
-    cridentials_age = cur_detail("_CERT_DT_")
+    try:
+        with open('./Data_Raw/CUR_user.json', 'r') as openfile:  # Reading from json file
+            cur_detail = json.load(openfile)
+        cridentials_age = cur_detail["_CERT_DT_"]
 
-    cridentials_age = cridentials_age.split(',')
-    cur_date = now_date.strftime("%Y,%m,%d")
-    cur_date = cur_date.split(',')
+        cridentials_age = cridentials_age.split(',')
+        cur_date = now_date.strftime("%Y,%m,%d")
+        cur_date = cur_date.split(',')
 
-    y = int(cur_date[0]) - int(cridentials_age[0])
-    m =  int(cur_date[1]) - int(cridentials_age[1])
-    d = int(cur_date[2]) - int(cridentials_age[2])
-    print("y", y)
-    print("y", m)
-    print("y", d)
+        y = int(cur_date[0]) - int(cridentials_age[0])
+        m =  int(cur_date[1]) - int(cridentials_age[1])
+        d = int(cur_date[2]) - int(cridentials_age[2])
+        print("y", y)
+        print("y", m)
+        print("y", d)
 
-    if y == 0:
-        m = m * 30
-        aged = m + d
-        if aged < 30:
-            print("aged: ", aged)
-            User_Email = cur_detail['_E_token_']
-            User_Pass = cur_detail['_P_token_']
-            User_Home_page(root)
+        if y == 0:
+            m = m * 30
+            aged = m + d
+            if aged < 30:
+                print("aged: ", aged)
+                User_Email = decrypt_data(cur_detail['_E_token_'])
+                User_Pass = decrypt_data(cur_detail['_P_token_'])
+                User_Home_page(root)
 
-    #except Exception as e:
-    #    print(e)
+    except Exception as e:
+        print(e)
 
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
--- test2.py
+++ test2.py
@@ -3,8 +3,8 @@ now_date = datetime.datetime.now()
 with open('./Data_Raw/CUR_user.json', 'r') as openfile:  # Reading from json file
     cur_detail = json.load(openfile)
 print(cur_detail)
-print(cur_detail("_CERT_DT_"))
-cridentials_age = cur_detail["_CERT_DT_")
+
+cridentials_age = cur_detail["_CERT_DT_"]
 
 cridentials_age = cridentials_age.split(',')
 cur_date = now_date.strftime("%Y,%m,%d")
@@ -12,7 +12,7 @@ cur_date = cur_date.split(',')
 
 y = int(cur_date[0]) - int(cridentials_age[0])
 m = int(cur_date[1]) - int(cridentials_age[1])
-d = int(cur_date[2]) - int(cridentials_age[2])
+d = iint(cur_date[2]) - int(cridentials_age[2])
 print("y", y)
 print("y", m)
 print("y", d)
